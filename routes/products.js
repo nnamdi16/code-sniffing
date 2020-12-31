22fe5ddd001a1234710449e278550c07a2cd0585
Nnamdi <nnwabuokei@paga.com>
2020-12-31 19:44:21 +0100

    implemented addToCart

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const axios = require('axios');
 const jsdom = require('jsdom');
+const { randomBytes } = require('crypto');
 const {JSDOM} = jsdom;
 const router = express.Router();
 const searchBaseUrl = `https://www.shopdisney.co.uk/search?q=`;
@@ -10,33 +11,64 @@ router.get('/', (req,res) => res.send("Product Search App loading"));
 
 router.get('/:search/:quantity', async (req,res) => {
         try {
-            console.log(req.params);
+
             const searchItem = req.params.search;
+            const quantity = req.params.quantity;
+
             let keyword = searchItem.replace(/,/g, '%2C');
             const searchString = keyword.split(' ').join('+');
             const url = `${searchBaseUrl}${searchString}`;
-            console.log(url);
+
+
+            //Search for the product 
             const response = await axios.get(url);
             console.log(Object.keys(response));
             const {data} = response;
+
+            //Convert the string html data to object 
             let doc = new JSDOM(data,{runScripts: "dangerously",resources: "usable"});
             const {window,window:{site_datalayer}} = doc;
-            // console.log(site_datalayer);
+
             const{product} = site_datalayer;
             const{product_id, product_availability } = product
-            console.log(product);
-            const state = false
+
+
+
+            //Check if the product derieved from search is not a list of items and is not out of stock 
+            let cartDetails;
             if (product !== undefined) {
                 if (product_availability === 'online - IN_STOCK') {
-                    const quantity = req.params.quantity;
-                    const cartBaseUrl = `https://www.shopdisney.co.uk/on/demandware.store/Sites-disneyuk-Site/en_GB/Product-AddedToCart?pid=${product_id}&quantity=${quantity}&personalised=${state}`;
-                    const response = await axios.get(cartBaseUrl);
-                    console.log("We are here");
-                    console.log(Object.keys(response));
+
+         
+                    // Request body to add the product to cart 
+                    const requestBody = {
+                        format:"ajax",
+                        quantity,
+                        pid:product_id,
+                        csrf_token:randomBytes(100).toString('base64')
+                    }
+
+                    const cartBaseUrl = `https://www.shopdisney.co.uk/on/demandware.store/Sites-disneyuk-Site/en_GB/Cart-AddProduct`;
+
+
+                    try {
+                        const response = await axios.post(cartBaseUrl,requestBody);
+                        const {data} = response;
+                        console.log(data)
+                        cartDetails = data;
+                        // const {data} = response;
+                        // let doc = new JSDOM(data,{runScripts: "dangerously",resources: "usable"});
+                        // const {window,window:{site_datalayer}} = doc;
+                        // console.log(window);
+                    } catch (error) {
+                        console.log(error);
+                    }
+
                 }
             }
             res.status(200).json({
-                site_datalayer
+                site_datalayer,
+                cartDetails
             });
         } catch (error) {
             console.log(error);
