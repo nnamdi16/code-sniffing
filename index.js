b12143072c420897eda877af683fcadf5f138970
Nnamdi <nnwabuokei@paga.com>
2020-12-31 16:20:15 +0100

    express server setup

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+require('./routes')(app);
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => console.log(`Listening on port ${port} `));
+
+module.exports = server;
+
