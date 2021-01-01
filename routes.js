9a096ca96109ff61f5cdfd19548eaad92234417a
Nnamdi <nnwabuokei@paga.com>
2021-01-01 11:21:33 +0100

    added cors and cookie parser

--- routes.js
+++ routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const Product = require('./routes/products');
 
 
 module.exports = (app) => {
     app.use(express.json());
+    app.use(cookieParser());
+    //Cors support
+    app.use(cors())
     app.use('/api', Product);
 }
